0b183e0a720bfac7b692873cad027addf273a735
kfir manor <kfir.manor8@gmail.com>
2023-02-15 14:31:26 +0200

    hi

---
+++ qga/vss-win32/log.cpp
@@ -0,0 +1,116 @@
+
+//  This application opens a file specified by the user and uses
+//  a temporary file to convert the file to upper case letters.
+//  Note that the given source file is assumed to be an ASCII text file
+//  and the new file created is overwritten each time the application is
+//  run.
+//
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#define BUFSIZE 1024
+
+void PrintError(LPCTSTR errDesc);
+
+int _tmain(int argc, TCHAR *argv[])
+{
+    HANDLE hTempFile = INVALID_HANDLE_VALUE; 
+
+    BOOL fSuccess  = FALSE;
+    DWORD dwRetVal = 0;
+    UINT uRetVal   = 0;
+
+    DWORD dwBytesRead    = 0;
+    DWORD dwBytesWritten = 0; 
+
+    TCHAR szTempFileName[MAX_PATH];  
+    TCHAR lpTempPathBuffer[MAX_PATH];
+    char  chBuffer[BUFSIZE]="HELLo world"; 
+    LPCTSTR errMsg;
+
+     //  Gets the temp path env string (no guarantee it's a valid path).
+    dwRetVal = GetTempPath(MAX_PATH,          // length of the buffer
+                           lpTempPathBuffer); // buffer for path 
+    if (dwRetVal > MAX_PATH || (dwRetVal == 0))
+    {
+        PrintError(TEXT("GetTempPath failed"));
+        return (2);
+    }
+
+    //  Generates a temporary file name. 
+    uRetVal = GetTempFileName(lpTempPathBuffer, // directory for tmp files
+                              TEXT("DEMO"),     // temp file name prefix 
+                              0,                // create unique name 
+                              szTempFileName);  // buffer for name 
+    if (uRetVal == 0)
+    {
+        PrintError(TEXT("GetTempFileName failed"));
+        return (3);
+    }
+
+    // create file
+    hTempFile = CreateFile((LPTSTR) szTempFileName, // file name 
+                           GENERIC_WRITE,        // open for write 
+                           0,                    // do not share 
+                           NULL,                 // default security 
+                           CREATE_ALWAYS,        // overwrite existing
+                           FILE_ATTRIBUTE_NORMAL,// normal file 
+                           NULL);                // no template 
+    if (hTempFile == INVALID_HANDLE_VALUE) 
+    { 
+        PrintError(TEXT("CreateFile failed"));
+        return (4);
+    } 
+
+    fSuccess = WriteFile(hTempFile, 
+                            chBuffer, 
+                            dwBytesRead,
+                            &dwBytesWritten, 
+                            NULL); 
+    if (!fSuccess) 
+    {
+        PrintError(TEXT("WriteFile failed"));
+        return (5);
+    }
+
+    
+    if (!CloseHandle(hTempFile)) 
+    {
+       PrintError(TEXT("CloseHandle(hTempFile) failed"));
+       return (8);
+    }
+
+    _tprintf(TEXT("All-caps version of %s written to AllCaps.txt\n"), "texe");
+    return (0);
+}
+
+//  ErrorMessage support function.
+//  Retrieves the system error message for the GetLastError() code.
+//  Note: caller must use LocalFree() on the returned LPCTSTR buffer.
+LPCTSTR ErrorMessage(DWORD error) 
+{ 
+    LPVOID lpMsgBuf;
+
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER 
+                   | FORMAT_MESSAGE_FROM_SYSTEM 
+                   | FORMAT_MESSAGE_IGNORE_INSERTS,
+                  NULL,
+                  error,
+                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                  (LPTSTR) &lpMsgBuf,
+                  0,
+                  NULL);
+
+    return((LPCTSTR)lpMsgBuf);
+}
+
+//  PrintError support function.
+//  Simple wrapper function for error output.
+void PrintError(LPCTSTR errDesc)
+{
+        LPCTSTR errMsg = ErrorMessage(GetLastError());
+        _tprintf(TEXT("\n** ERROR ** %s: %s\n"), errDesc, errMsg);
+        LocalFree((LPVOID)errMsg);
+}
