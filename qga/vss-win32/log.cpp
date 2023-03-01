91f2e8313b4fab0cad94c9ef08c6bea39932b59a
kfir manor <kfir.manor8@gmail.com>
2023-03-01 10:34:35 +0200

    dsa

--- qga/vss-win32/log.cpp
+++ qga/vss-win32/log.cpp
@@ -1,9 +1,9 @@
 #include "qemu/osdep.h"
 #include "log.h"
 
-
 typedef struct LogConfig{
     char log_filepath[MAX_PATH];
+    GLogLevelFlags log_level_mask;
 } LogConfig;
 
 typedef struct LogState{
@@ -13,7 +13,37 @@ typedef struct LogState{
 LogConfig *log_config;
 LogState *log_state;
 
-bool set_log_filepath(char * p){
+DWORD get_reg_dword_value(HKEY baseKey, LPCSTR subKey, LPCSTR valueName,
+                          DWORD defaultData)
+{
+    DWORD regGetValueError;
+    DWORD dwordData;
+    DWORD dataSize = sizeof(DWORD);
+
+    regGetValueError = RegGetValue(baseKey, subKey, valueName, RRF_RT_DWORD,
+                                   NULL, &dwordData, &dataSize);
+    if (regGetValueError  != ERROR_SUCCESS) {
+        return defaultData;
+    }
+    return dwordData;
+}
+
+GLogLevelFlags get_log_level_mask(DWORD log_level){
+    GLogLevelFlags default_mask=G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL 
+                                | G_LOG_LEVEL_WARNING;
+    if(log_level > 0) {
+        default_mask= default_mask | G_LOG_LEVEL_MESSAGE;
+    }
+    if(log_level > 1) {
+        default_mask= default_mask | INFO;
+    }
+    if(log_level > 2) {
+        default_mask= default_mask | DEBUG;
+    }
+    return default_mask
+}
+
+bool set_tmp_filepath(char * p){
     DWORD dwRetVal = 0;
     UINT uRetVal   = 0;
     TCHAR lpTempPathBuffer[MAX_PATH];
@@ -61,7 +91,13 @@ void file_log(FILE *log_file,const char *level_str,const gchar* message)
     fflush(log_file);
 }
 
-void vss_log(const gchar *log_domain, GLogLevelFlags log_level,
+void inactive_vss_log(const gchar *log_domain, GLogLevelFlags log_level,
+                     const gchar *message, gpointer user_data)
+{
+    return;
+}
+
+void active_vss_log(const gchar *log_domain, GLogLevelFlags log_level,
                      const gchar *message, gpointer user_data)
 {
     LogState *log_state = (LogState *)user_data;
@@ -82,13 +118,24 @@ static FILE *open_logfile(const char *logfilepath)
     return f;
 }
 
-void init_vss_log(void){
+void init_vss_log(void)
+{
     log_config= g_new0(LogConfig, 1);
     log_state= g_new0(LogState, 1);
     log_state->log_file = stderr;
-    g_log_set_handler(G_LOG_DOMAIN,G_LOG_LEVEL_MASK,vss_log, log_state);
-
-    if (set_log_filepath(log_config->log_filepath)) {
+    log_config->log_level_mask = get_log_level_mask(
+                                 get_reg_dword_value(HKEY_LOCAL_MACHINE,
+                                                QGA_PROVIDER_REGISTRY_ADDRESS,
+                                                 "LogLevel", 0))
+    g_log_set_handler(G_LOG_DOMAIN, log_config->log_level_mask,
+                      active_vss_log, log_state);
+    g_log_set_handler(G_LOG_DOMAIN, ~(log_config->log_level_mask),
+                      inactive_vss_log, NULL);
+    g_info("im alive");
+    g_message("im alive");
+    g_debug("im alive");
+    g_warning("im alive");
+    if (set_tmp_filepath(log_config->log_filepath)) {
             printf("oppening file: %s",log_config->log_filepath);
             FILE *log_file = open_logfile(log_config->log_filepath);
             if (!log_file) {
@@ -100,8 +147,8 @@ void init_vss_log(void){
     }
 }
 
-void cleanup_vss_log(void){
+void cleanup_vss_log(void)
+{
     g_free(log_config);
     g_free(log_state);
 }
-
