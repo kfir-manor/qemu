642f27735bda8ee3ed5e7f8322914513156cadcb
kfir manor <kfir.manor8@gmail.com>
2023-03-15 14:32:35 +0200

    uoh

--- qga/qga-log.c
+++ qga/qga-log.c
@@ -20,7 +20,7 @@ static void ga_log(const gchar *domain, GLogLevelFlags level,
                    const gchar *msg, gpointer opaque)
 {
     GAState *s = opaque;
-    const char *level_str = ga_log_level_str(level);
+    const char *level_str = log_level_str(level);
 
     if (!ga_logging_enabled(s)) {
         return;
@@ -89,7 +89,7 @@ void init_log(GAState *s,GAConfig *cofig){
             become_daemon(config->pid_filepath);
         }
         if (config->log_filepath) {
-            FILE *log_file = ga_open_logfile(config->log_filepath);
+            FILE *log_file = open_logfile(config->log_filepath);
             if (!log_file) {
                 g_critical("unable to open specified log file: %s",
                            strerror(errno));
