fe121de418c95dc33bcb41076bdbb83c70593291
Kfir Manor <kfir.manor8@gmail.com>
2023-03-20 21:47:37 +0200

    ads

--- qga/vss-win32/vss-log.h
+++ qga/vss-win32/vss-log.h
@@ -1,5 +1,10 @@
 #undef G_LOG_DOMAIN
 #define G_LOG_DOMAIN "QGA VSS SERVICE"
 
+typedef struct LogState {
+    FILE *log_file;
+    bool logging_enabled;
+} LogState;
+
 void init_vss_log(void);
 void cleanup_vss_log(void);
