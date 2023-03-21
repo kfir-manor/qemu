c56a6199fc0d5d050fea7d50f6e3a5eaffe5e4ef
Kfir Manor <kfir.manor8@gmail.com>
2023-03-21 21:13:06 +0200

    das

--- qga/vss-win32/vss-log.h
+++ qga/vss-win32/vss-log.h
@@ -1,20 +1,11 @@
 #undef G_LOG_DOMAIN
 #define G_LOG_DOMAIN "QGA VSS SERVICE"
 
-#define LOG_FILE_NAME "qga_vss_log.log"
 
-typedef struct LogConfig {
-    char log_filepath[MAX_PATH + strlen(LOG_FILE_NAME)];
-    GLogLevelFlags log_level_mask;
-} LogConfig;
+typedef struct LogConfig LogConfig;
 
-typedef struct LogState {
-    FILE *log_file;
-    bool logging_enabled;
-} LogState;
+typedef struct LogState LogState;
 
-LogConfig *log_config;
-LogState  *log_state;
 
 void init_vss_log(void);
 void cleanup_vss_log(void);
