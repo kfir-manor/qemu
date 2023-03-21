ea5ef2ff94f1def8831bf5f7c0c44e386bf3ae8f
Kfir Manor <kfir.manor8@gmail.com>
2023-03-21 19:51:21 +0200

    das

--- qga/vss-win32/vss-log.cpp
+++ qga/vss-win32/vss-log.cpp
@@ -8,19 +8,6 @@
 #define LOG_FILE_NAME "qga_vss_log.log"
 
 
-typedef struct LogConfig {
-    char log_filepath[MAX_PATH + strlen(LOG_FILE_NAME)];
-    GLogLevelFlags log_level_mask;
-} LogConfig;
-
-typedef struct LogState {
-    FILE *log_file;
-    bool logging_enabled;
-} LogState;
-
-LogConfig *log_config;
-LogState *log_state;
-
 void freeze_log(void){
     log_state->logging_enabled=false;
 }
