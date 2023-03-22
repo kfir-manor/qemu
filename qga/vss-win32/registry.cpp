02298a8f67d414b4d4bf8cb9fdb40685fad91299
kfir manor <kfir.manor8@gmail.com>
2023-03-22 12:59:27 +0200

    dsa

--- qga/vss-win32/registry.cpp
+++ qga/vss-win32/registry.cpp
@@ -1,6 +1,4 @@
-#include "qemu/osdep.h"
-#include "vss-common.h"
-
+#include "registry.h"
 DWORD get_reg_dword_value(HKEY baseKey, LPCSTR subKey, LPCSTR valueName,
                           DWORD defaultData)
 {
