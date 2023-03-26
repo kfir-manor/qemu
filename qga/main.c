4393a206ddb620c30a098525ffdefd541854bfd9
Kfir Manor <kfir.manor8@gmail.com>
2023-03-26 18:59:22 +0300

    dsa

--- qga/main.c
+++ qga/main.c
@@ -278,25 +278,6 @@ QEMU_HELP_BOTTOM "\n"
     dfl_pathnames.state_dir);
 }
 
-static const char *ga_log_level_str(GLogLevelFlags level)
-{
-    switch (level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:
-        return "error";
-    case G_LOG_LEVEL_CRITICAL:
-        return "critical";
-    case G_LOG_LEVEL_WARNING:
-        return "warning";
-    case G_LOG_LEVEL_MESSAGE:
-        return "message";
-    case G_LOG_LEVEL_INFO:
-        return "info";
-    case G_LOG_LEVEL_DEBUG:
-        return "debug";
-    default:
-        return "user";
-    }
-}
 
 bool ga_logging_enabled(GAState *s)
 {
@@ -313,43 +294,12 @@ void ga_enable_logging(GAState *s)
     s->logging_enabled = true;
 }
 
-static int glib_log_level_to_system(int level)
-{
-    switch (level) {
-#ifndef _WIN32
-    case G_LOG_LEVEL_ERROR:
-        return LOG_ERR;
-    case G_LOG_LEVEL_CRITICAL:
-        return LOG_CRIT;
-    case G_LOG_LEVEL_WARNING:
-        return LOG_WARNING;
-    case G_LOG_LEVEL_MESSAGE:
-        return LOG_NOTICE;
-    case G_LOG_LEVEL_DEBUG:
-        return LOG_DEBUG;
-    case G_LOG_LEVEL_INFO:
-    default:
-        return LOG_INFO;
-#else
-    case G_LOG_LEVEL_ERROR:
-    case G_LOG_LEVEL_CRITICAL:
-        return EVENTLOG_ERROR_TYPE;
-    case G_LOG_LEVEL_WARNING:
-        return EVENTLOG_WARNING_TYPE;
-    case G_LOG_LEVEL_MESSAGE:
-    case G_LOG_LEVEL_INFO:
-    case G_LOG_LEVEL_DEBUG:
-    default:
-        return EVENTLOG_INFORMATION_TYPE;
-#endif
-    }
-}
 
 static void ga_log(const gchar *domain, GLogLevelFlags level,
                    const gchar *msg, gpointer opaque)
 {
     GAState *s = opaque;
-    const char *level_str = ga_log_level_str(level);
+    const char *level_str = log_level_str(level);
 
     if (!ga_logging_enabled(s)) {
         return;
@@ -358,16 +308,12 @@ static void ga_log(const gchar *domain, GLogLevelFlags level,
     level &= G_LOG_LEVEL_MASK;
     if (g_strcmp0(domain, "syslog") == 0) {
 #ifndef _WIN32
-        syslog(glib_log_level_to_system(level), "%s: %s", level_str, msg);
+        system_log(level,level_str,msg);
 #else
-        ReportEvent(s->event_log, glib_log_level_to_system(level),
-                    0, 1, NULL, 1, 0, &msg, NULL);
+        win_system_log(s->event_log,level,msg);
 #endif
     } else if (level & s->log_level) {
-        g_autoptr(GDateTime) now = g_date_time_new_now_utc();
-        g_autofree char *nowstr = g_date_time_format(now, "%s.%f");
-        fprintf(s->log_file, "%s: %s: %s\n", nowstr, level_str, msg);
-        fflush(s->log_file);
+        file_log(s->log_file,level_str,msg);
     }
 }
 
@@ -376,18 +322,6 @@ void ga_set_response_delimited(GAState *s)
     s->delimit_response = true;
 }
 
-static FILE *ga_open_logfile(const char *logfile)
-{
-    FILE *f;
-
-    f = fopen(logfile, "a");
-    if (!f) {
-        return NULL;
-    }
-
-    qemu_set_cloexec(fileno(f));
-    return f;
-}
 
 static gint ga_strcmp(gconstpointer str1, gconstpointer str2)
 {
@@ -479,7 +413,7 @@ void ga_unset_frozen(GAState *s)
      * in a frozen state at start up, do it now
      */
     if (s->deferred_options.log_filepath) {
-        s->log_file = ga_open_logfile(s->deferred_options.log_filepath);
+        s->log_file = open_logfile(s->deferred_options.log_filepath);
         if (!s->log_file) {
             s->log_file = stderr;
         }
@@ -1356,7 +1290,7 @@ static GAState *initialize_agent(GAConfig *config, int socket_activation)
             become_daemon(config->pid_filepath);
         }
         if (config->log_filepath) {
-            FILE *log_file = ga_open_logfile(config->log_filepath);
+            FILE *log_file = open_logfile(config->log_filepath);
             if (!log_file) {
                 g_critical("unable to open specified log file: %s",
                            strerror(errno));
