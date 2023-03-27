4cd5e778ee473a11a7c06b9a484a522dba39a570
Kfir Manor <kfir.manor8@gmail.com>
2023-03-27 18:42:34 +0300

    asd

--- qga/vss-win32/vss-log.h
+++ qga/vss-win32/vss-log.h
@@ -5,7 +5,5 @@ void init_vss_log(void);
 void cleanup_vss_log(void);
 void disable_log(void);
 void enable_log(void);
-#define g_critical_error_pretty(win32_err,fmt,...) \
-    g_critical_error_pretty_internal((win32_err), (fmt), ## __VA_ARGS__)
-void g_critical_error_pretty_internal(int win32_err,const char *fmt,...) G_GNUC_PRINTF(2, 3);
+void g_critical_error_pretty(int win32_err,const char *fmt,...) G_GNUC_PRINTF(2, 3);
 
