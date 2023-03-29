52c63638dd52a90451ec86614daf108649201ad8
kfir manor <kfir.manor8@gmail.com>
2023-03-29 12:20:38 +0300

    dsa

--- qga/vss-win32/registry.cpp
+++ qga/vss-win32/registry.cpp
@@ -11,8 +11,8 @@ DWORD get_reg_dword_value(HKEY baseKey, LPCSTR subKey, LPCSTR valueName,
                                    NULL, &dwordData, &dataSize);
     if (regGetValueError  != ERROR_SUCCESS) {
         g_win32_error_log_warning(regGetValueError,
-                                  "failed to get RegValue %s form %s/%s",
-                                  valueName, baseKey, subKey);
+                                  "failed to get RegValue %s form %s",
+                                  valueName, subKey);
         return defaultData;
     }
     return dwordData;
