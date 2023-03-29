ad8864406ccb3c7c466b1f63edfe60eef614844f
kfir manor <kfir.manor8@gmail.com>
2023-03-29 13:47:17 +0300

    dsadsa

--- qga/guest-agent-core.h
+++ qga/guest-agent-core.h
@@ -32,6 +32,9 @@ void ga_command_state_add(GACommandState *cs,
 void ga_command_state_init_all(GACommandState *cs);
 void ga_command_state_cleanup_all(GACommandState *cs);
 GACommandState *ga_command_state_new(void);
+bool ga_logging_enabled(GAState *s);
+void ga_disable_logging(GAState *s);
+void ga_enable_logging(GAState *s);
 void ga_command_state_free(GACommandState *cs);
 void G_GNUC_PRINTF(1, 2) slog(const gchar *fmt, ...);
 void ga_set_response_delimited(GAState *s);
