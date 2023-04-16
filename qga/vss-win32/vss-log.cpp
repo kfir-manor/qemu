ad9e58636440e38fbad84f63fe9ce678f8c6d0ab
Kfir Manor <kfir.manor8@gmail.com>
2023-04-16 10:00:26 +0300

    qga/win/vss:add glib log handler
    adds a glib log handler to qga vss on the "QGA VSS SERVICE" log domain
    
    log file "qga_vss.log" will be placed in a temp folder
    
    Signed-off-by: Kfir Manor <kfir@daynix.com>

---
+++ qga/vss-win32/vss-log.cpp
@@ -0,0 +1,80 @@
+#include "registry.h"
+#include "vss-handles.h"
+extern "C" {
+#include "qga/log-utils.h"
+}
+#include "vss-log.h"
+
+#define LOG_FILE_NAME "qga_vss.log"
+
+typedef struct LogConfig {
+    char log_filepath[MAX_PATH + strlen(LOG_FILE_NAME)];
+} LogConfig;
+
+typedef struct LogState {
+    FILE *log_file;
+} LogState;
+
+static LogConfig *log_config;
+static LogState *log_state;
+
+bool set_tmp_file_path(char *p)
+{
+    DWORD dwRetVal = 0;
+    dwRetVal = GetTempPath(MAX_PATH, p);
+    if (dwRetVal > MAX_PATH || (dwRetVal == 0)) {
+        g_critical("GetTempPath failed");
+        return false;
+    }
+    strcat(p, LOG_FILE_NAME);
+    return true;
+}
+
+void vss_log(const gchar *log_domain, GLogLevelFlags log_level,
+                    const gchar *message, gpointer user_data)
+{
+    LogState *s = (LogState *)user_data;
+    const char *level_str = ga_log_level_str(log_level);
+    file_log(s->log_file, level_str, message);
+}
+
+void init_vss_log(void)
+{
+    log_config = g_new0(LogConfig, 1);
+    log_state = g_new0(LogState, 1);
+    log_state->log_file = stderr;
+    g_log_set_handler(G_LOG_DOMAIN, G_LOG_LEVEL_MASK, vss_log,
+                      log_state);
+    if (set_tmp_file_path(log_config->log_filepath)) {
+        printf("oppening file: %s\n", log_config->log_filepath);
+        FILE *tmp_log_file = ga_open_logfile(log_config->log_filepath);
+        if (!tmp_log_file) {
+            printf("unable to open specified log file: %s\n", strerror(errno));
+            return;
+        }
+        *(log_state->log_file) = *tmp_log_file;
+    }
+}
+
+void deinit_vss_log(void)
+{
+    g_free(log_config);
+    g_free(log_state);
+<<<<<<< HEAD
+}
+=======
+    is_log_init = false;
+}
+
+void win32_error_log(int win32_err, GLogLevelFlags log_level, const char *fmt,
+                       ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    char *msg = g_strdup_vprintf(fmt, ap);
+    char *suffix = g_win32_error_message(win32_err);
+    g_log(G_LOG_DOMAIN, log_level, "%s: %s", msg, suffix);
+    g_free(suffix);
+    va_end(ap);
+}
+>>>>>>> ad86a7c5052 (qga-win-vss-add-glib-log-handler fix)
---
+++ qga/vss-win32/vss-log.h
@@ -0,0 +1,5 @@
+#undef G_LOG_DOMAIN
+#define G_LOG_DOMAIN "QGA VSS SERVICE"
+
+void init_vss_log(void);
+void deinit_vss_log(void);
