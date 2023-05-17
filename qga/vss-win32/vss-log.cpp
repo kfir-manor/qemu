6f5bfc5e0edb6058f4a35a63b476cd35ebf8fc70
kfir manor <kfir.manor8@gmail.com>
2023-05-17 14:14:07 +0300

    qga/win/vss: add log level control
    log level of handler can be controlled by logLevel(QEMU Guest Agent VSS Provider service registry value)
    level   added logs
    0.      error, critical, warning
    1.      message
    2.      info
    3.      debug
    
    The default log level is 0 (when the logLevel value isn't available)
    
    Signed-off-by: Kfir Manor <kfir@daynix.com>

--- qga/vss-win32/vss-log.cpp
+++ qga/vss-win32/vss-log.cpp
@@ -5,10 +5,13 @@ extern "C" {
 }
 #include "vss-log.h"
 
+#define DEFAULT_LOG_LEVEL_MASK \
+    (G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL | G_LOG_LEVEL_WARNING)
 #define LOG_FILE_NAME "qga_vss.log"
 
 typedef struct LogConfig {
     char log_filepath[MAX_PATH + strlen(LOG_FILE_NAME)];
+    GLogLevelFlags log_level_mask;
 } LogConfig;
 
 typedef struct LogState {
@@ -17,6 +20,32 @@ typedef struct LogState {
 
 static LogConfig *log_config;
 static LogState *log_state;
+
+DWORD get_log_level(void)
+{
+    return get_reg_dword_value(HKEY_LOCAL_MACHINE,
+                               QGA_PROVIDER_REGISTRY_ADDRESS, "LogLevel", 0);
+}
+
+GLogLevelFlags convert_log_level_to_mask(DWORD log_level)
+{
+    int mask = DEFAULT_LOG_LEVEL_MASK;
+    if (log_level > 0) {
+        mask |= G_LOG_LEVEL_MESSAGE;
+    }
+    if (log_level > 1) {
+        mask |= G_LOG_LEVEL_INFO;
+    }
+    if (log_level > 2) {
+        mask |= G_LOG_LEVEL_DEBUG;
+    }
+    return static_cast<GLogLevelFlags>(mask);
+}
+
+GLogLevelFlags get_log_level_mask(void)
+{
+    return convert_log_level_to_mask(get_log_level());
+}
 
 bool set_tmp_file_path(char *p)
 {
@@ -33,6 +62,9 @@ bool set_tmp_file_path(char *p)
 void vss_log(const gchar *log_domain, GLogLevelFlags log_level,
                     const gchar *message, gpointer user_data)
 {
+    if (!(log_config->log_level_mask & log_level)) {
+        return;
+    }
     LogState *s = (LogState *)user_data;
     const char *level_str = ga_log_level_str(log_level);
     file_log(s->log_file, level_str, message);
@@ -42,7 +74,9 @@ void init_vss_log(void)
 {
     log_config = g_new0(LogConfig, 1);
     log_state = g_new0(LogState, 1);
+    log_state->logging_enabled = true;
     log_state->log_file = stderr;
+    log_config->log_level_mask = get_log_level_mask();
     g_log_set_handler(G_LOG_DOMAIN, G_LOG_LEVEL_MASK, vss_log,
                       log_state);
     if (set_tmp_file_path(log_config->log_filepath)) {
