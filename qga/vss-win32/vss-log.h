383d9af1f22f56a2bac79ca97c33060f6d38f049
kfir manor <kfir.manor8@gmail.com>
2023-05-17 14:28:14 +0300

    qga/win/vss: add win32 error log
    wrapper function that is added to simplify logging of win32 errors by turning the error to redable string inside the function
    
    Signed-off-by: Kfir Manor <kfir@daynix.com>

--- qga/vss-win32/vss-log.h
+++ qga/vss-win32/vss-log.h
@@ -5,3 +5,9 @@ void init_vss_log(void);
 void deinit_vss_log(void);
 void disable_log(void);
 void enable_log(void);
+#define win32_error_log_critical(win32_err, fmt, ...) \
+    win32_error_log((win32_err), G_LOG_LEVEL_CRITICAL, (fmt), ##__VA_ARGS__)
+#define win32_error_log_warning(win32_err, fmt, ...) \
+    win32_error_log((win32_err), G_LOG_LEVEL_WARNING, (fmt), ##__VA_ARGS__)
+void win32_error_log(int win32_err, GLogLevelFlags log_level, const char *fmt,
+                          ...) G_GNUC_PRINTF(3, 4);
