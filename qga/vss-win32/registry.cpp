322260e7497c3bd44e2103c3c3402ebbcda22a38
kfir manor <kfir.manor8@gmail.com>
2023-05-17 12:58:35 +0300

    qga/win/vss: refactor get_reg_dword_value
    move get_reg_dword_value to a separate file for later reuse in qga vss log
    
    Signed-off-by: Kfir Manor <kfir@daynix.com>

---
+++ qga/vss-win32/registry.cpp
@@ -0,0 +1,16 @@
+#include "registry.h"
+
+DWORD get_reg_dword_value(HKEY baseKey, LPCSTR subKey, LPCSTR valueName,
+                          DWORD defaultData)
+{
+    DWORD regGetValueError;
+    DWORD dwordData;
+    DWORD dataSize = sizeof(DWORD);
+
+    regGetValueError = RegGetValue(baseKey, subKey, valueName, RRF_RT_DWORD,
+                                   NULL, &dwordData, &dataSize);
+    if (regGetValueError != ERROR_SUCCESS) {
+        return defaultData;
+    }
+    return dwordData;
+}
