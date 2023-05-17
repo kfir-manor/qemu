9ec0f32b5f66c1b2318f732481dd14fffc7fec94
kfir manor <kfir.manor8@gmail.com>
2023-05-17 10:33:16 +0300

    hi

--- qga/log-utils.h
+++ qga/log-utils.h
@@ -1,5 +1,13 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include "qemu/osdep.h"
 
 const char *ga_log_level_str(GLogLevelFlags level);
 FILE *ga_open_logfile(const char *logfile);
 void file_log(FILE *log_file, const char *level_str, const gchar *msg);
+#ifdef __cplusplus
+}
+#endif
